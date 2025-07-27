b8725c3f888f784087a4720bef61fccf105bb7da
rawooldipesh <rawooldipesh0@gmail.com>
2025-07-27 15:24:05 +0530

    Deploy ready

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-if-env-missing
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['inventory-tracker-bpju.onrender.com']
 
 
 # Application definition
